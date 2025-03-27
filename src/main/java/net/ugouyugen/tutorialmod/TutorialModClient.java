c9deb09bac6ecd6a5eeb83aba4bcb02c043d1c77
MasakiGohara <masaki.mountain@gmail.com>
2025-03-27 23:59:45 +0900

    Initial commit

---
+++ src/main/java/net/ugouyugen/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.ugouyugen.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
